a5435e50b64c6f6f9d36f3459347c9e8d27231f0
gemenerik <rikjbouwmeester@gmail.com>
2021-11-11 16:58:43 +0100

    Implement SLIP packet decoding state machine

--- src/drivers/esp32/src/esp_slip.c
+++ src/drivers/esp32/src/esp_slip.c
@@ -31,6 +31,26 @@
 
 #define ESP_OVERHEAD_LEN 8
 
+typedef enum
+{
+    receiveStart,
+    receiveDirection,
+    receiveCommand,
+    receiveSize,
+    receiveValue,
+    receiveData,
+    receiveEnd,
+    error,
+} ESPblReceiveState;
+static ESPblReceiveState espblReceiveState = receiveStart;
+
+static uint8_t size[2];
+static uint8_t value[4];
+static uint8_t data_index = 0;
+static uint8_t size_index = 0;
+static uint8_t value_index = 0;
+static uint8_t previous_db = 0;
+
 static uint8_t sendBuffer[ESP_MTU + 10 + 16];
 static uint32_t sendSize;
 
@@ -63,6 +83,143 @@ static void sendSLIPPacket(uint32_t size, uint8_t *data, coms_putchar_t putCharF
             putCharFnc(data[i]);
         }
     }
+}
+
+static slipDecoderStatus_t receiveSLIPPacket(uint8_t c, esp_uart_receive_packet *receiver_pckt, esp_uart_send_packet *sender_pckt)
+{
+
+    slipDecoderStatus_t decoderStatus = SLIP_DECODING;
+    switch (espblReceiveState)
+    {
+    case receiveStart:
+        receiver_pckt->status = 1;
+        espblReceiveState = (c == 0xC0) ? receiveDirection : receiveStart;
+        break;
+    case receiveDirection:
+        if (c == 0x01)
+        {
+            espblReceiveState = receiveCommand;
+        }
+        else
+        {
+            espblReceiveState = error;
+            decoderStatus = SLIP_ERROR;
+        }
+        break;
+    case receiveCommand:
+        receiver_pckt->command = c;
+        size_index = 0;
+        if (c == sender_pckt->command)
+        {
+            espblReceiveState = receiveSize;
+        }
+        else
+        {
+            espblReceiveState = error;
+            decoderStatus = SLIP_ERROR;
+        }
+        break;
+    case receiveSize:
+        size[size_index] = c;
+        if (size_index == 1)
+        {
+            receiver_pckt->data_size = ((uint16_t)size[0] + ((uint16_t)size[1] << 8));
+            value_index = 0;
+            if (receiver_pckt->data_size > 0 && receiver_pckt->data_size < ESP_MTU)
+            {
+                espblReceiveState = receiveValue;
+            }
+            else
+            {
+                espblReceiveState = error;
+                decoderStatus = SLIP_ERROR;
+            }
+        }
+        size_index++;
+        break;
+    case receiveValue: // only used for READ_REG
+        value[value_index] = c;
+        if (value_index == 3)
+        {
+            receiver_pckt->value = ((uint32_t)value[0] + ((uint32_t)value[1] << 8) + ((uint32_t)value[2] << 16) + ((uint32_t)value[3] << 24));
+            data_index = 0;
+            previous_db = 0;
+            espblReceiveState = receiveData;
+        }
+        value_index++;
+        break;
+    case receiveData:
+    {
+        const uint16_t payloadSize = receiver_pckt->data_size - 4;
+        if (data_index < payloadSize)
+        {
+            if (c == 0xDB)
+            {
+                previous_db = 1;
+            }
+            else if (previous_db)
+            {
+                previous_db = 0;
+                if (c == 0xDC)
+                {
+                    receiver_pckt->data[data_index] = 0xC0;
+                    data_index++;
+                }
+                else if (c == 0xDD)
+                {
+                    receiver_pckt->data[data_index] = 0xDB;
+                    data_index++;
+                }
+                else
+                {
+                    espblReceiveState = error;
+                }
+            }
+            else
+            {
+                receiver_pckt->data[data_index] = c;
+                data_index++;
+            }
+        }
+        else
+        {
+            if (data_index == payloadSize)
+            {
+                receiver_pckt->status = c;
+            }
+            if (data_index == payloadSize + 1)
+            {
+                receiver_pckt->error = c;
+            }
+            if (data_index == payloadSize + 3)
+            {
+                espblReceiveState = receiveEnd;
+            }
+            data_index++;
+        }
+    }
+    break;
+
+    case receiveEnd:
+        if (c == 0xC0)
+        {
+            espblReceiveState = receiveStart;
+            decoderStatus = SLIP_SUCCESS;
+        }
+        else
+        {
+            espblReceiveState = error;
+            decoderStatus = SLIP_ERROR;
+        }
+        break;
+
+    case error:
+        decoderStatus = SLIP_ERROR;
+        break;
+    default:
+        break;
+    }
+    return decoderStatus;
 }
 static void espblAssembleBuffer(esp_uart_send_packet *sender_pckt)
 {
